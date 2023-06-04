85ed6db163fe9252d4c6394a1d9710cd4f3747d3
AtsuyaOotsuka <atsuya@atylab.work>
2023-06-04 17:24:50 +0900

    初期テーブルマイグレーション

--- htdocs/database/migrations/2023_06_04_081216_create_todo_table.php
+++ htdocs/database/migrations/2023_06_04_081216_create_todo_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('todo', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->foreignId("user_id");
+            $table->string('task_name');
+            $table->boolean('is_finished');
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('todo');
     }
 };
